23a01e37e8be84bdb0f927c91acad9c441539a74
danishsheikh1122 <mohammeddanishsheikh1122@gmail.com>
2024-06-24 17:18:47 +0530

    implementing auth.js

--- app/api/auth/[...nextauth]/route.tsx
+++ app/api/auth/[...nextauth]/route.tsx
@@ -1,2 +1,20 @@
-import { handlers } from "@/auth" // Referring to the auth.ts we just created
-export const { GET, POST } = handlers
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import prisma from "@/prisma/client";
+
+const handler = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.AUTH_GOOGLE_ID!,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET!,
+    }),
+  ],
+
+  session: {
+    strategy: "jwt",
+  },
+});
+
+export { handler as GET, handler as POST };
